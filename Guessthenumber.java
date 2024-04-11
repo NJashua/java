eef2d93a915efbd55040ae3fa850b2dbe8f68285
NJashua <nithinjashua1@gmail.com>
2024-04-11 17:22:58 +0530

    Random number guessing

---
+++ Guessthenumber.java
@@ -0,0 +1,23 @@
+import java.sql.Struct;
+import java.util.Scanner;
+public class Guessthenumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your firstname: ");
+        String firstName = scanner.next();
+        System.out.println("Enter your lastname: ");
+        String lastName = scanner.next();
+        System.out.println("Enter your guess: ");
+        int randomNumber = (int) (Math.random() * 100);
+        // System.out.println(randomNumber); haha aatagallu..
+        int guessNum = scanner.nextInt();
+        
+        if (randomNumber == guessNum){
+            System.err.println(String.format("Hey %s %s you guess is correct..", firstName, lastName));
+        }
+        else{
+            System.out.println(String.format("Try again %s", firstName));
+        }
+        scanner.close();
+    }
+}
