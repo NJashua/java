d68719aa2ac53c8d51a8069f5926003b95ddc67a
NJashua <nithinjashua1@gmail.com>
2024-04-13 19:45:21 +0530

    Closed the scanner object

--- Asciivalue.java
+++ Asciivalue.java
@@ -3,27 +3,22 @@ By taking the user input and after type conversion..,
 import java.util.Scanner; */
 
 import java.util.Scanner;
+
 public class Asciivalue {
-public static void main(String[] args){
-    Scanner userValue = new Scanner(System.in);
-    System.out.print("Enter a Sentence : ");
-    String charValue = userValue.nextLine();
-    // System.out.println(charValue);
-    System.out.println(charValue.length());
-    System.out.print("Enter an index : ");
-    int pos = userValue.nextInt();
-    if (pos > 0 && pos < charValue.length() )
-    {
-        char Alphabet = charValue.charAt(pos);
-        int getAscii = (int) Alphabet;
-        System.out.println(String.format("The sentenc is:  %s \nand it's ASCII value of %s at index %d is: %d",charValue, Alphabet, pos));
-
+    public static void main(String[] args) {
+        Scanner userValue = new Scanner(System.in);
+        System.out.print("Enter a Sentence : ");
+        String charValue = userValue.nextLine();
+        System.out.println("Length of the sentence: " + charValue.length());
+        System.out.print("Enter an index : ");
+        int pos = userValue.nextInt();
+        if (pos >= 0 && pos < charValue.length()) {
+            char alphabet = charValue.charAt(pos);
+            int asciiValue = (int) alphabet;
+            System.out.println(String.format("The sentence is:  %s \nand its ASCII value of %s at index %d is: %d", charValue, alphabet, pos, asciiValue));
+        } else {
+            System.out.println("Enter the index within range: ");
+        }
+        userValue.close(); // Closing the Scanner object
     }
-    else {
-        System.out.println("Enter the index with in range: ");
-    }
-
-    
-}
-    
 }
